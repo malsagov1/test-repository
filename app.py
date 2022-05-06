553b5a290ef82fce022582763bf6b5035ee5e946
Umar Malsagov <umar06mals@gmail.com>
2022-05-06 17:55:12 +0200

    make it better

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ api.add_resource(Items, '/items' )
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
+db.init_app(app)
 if __name__ == "__main__":
-    db.init_app(app)
-    app.run(port=5000, debug = True)
+    app.run()
+        
